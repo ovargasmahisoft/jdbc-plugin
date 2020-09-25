03e6aba8264a470ac06b5aefab628b6fa61563ee
GitHub Action <action@github.com>
2020-09-25 02:20:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/ovargasmahisoft/jdbcplugin/MyBundle.kt
+++ src/main/kotlin/com/github/ovargasmahisoft/jdbcplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ovargasmahisoft.jdbcplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/ovargasmahisoft/jdbcplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ovargasmahisoft.jdbcplugin.services
+
+import com.github.ovargasmahisoft.jdbcplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
