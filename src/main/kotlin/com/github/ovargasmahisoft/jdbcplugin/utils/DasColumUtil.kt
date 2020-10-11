2fe131bd750fb57d2803ef60d683ef905b485f43
Orlando Vargas <4366999-ovargas1@users.noreply.gitlab.com>
2020-10-11 15:15:09 -0600

    refactor: removed unused functions

--- src/main/kotlin/com/github/ovargasmahisoft/jdbcplugin/utils/DasColumUtil.kt
+++ src/main/kotlin/com/github/ovargasmahisoft/jdbcplugin/utils/DasColumUtil.kt
@@ -1,8 +1,6 @@
 package com.github.ovargasmahisoft.jdbcplugin.utils
 
 import com.intellij.database.model.DasColumn
-import com.intellij.database.psi.DbTable
-import com.intellij.database.util.DasUtil
 
 fun DasColumn.daoJavaType(): String {
     return when (this.dataType.typeName) {
@@ -54,35 +52,4 @@ fun DasColumn.nullableResultSetMap(): String {
         "timestamp" -> "JdbcUtil.instantOrNull(rs, "
         else -> "rs.getObject("
     }
-}
-
-fun DasColumn.javaDefinition(): String {
-    return "    ${this.daoJavaType()} ${this.camelCaseName()};\n"
-}
-
-fun DasColumn.javaColumnMapper(): String {
-    return when (this.dataType.typeName) {
-        "bigint" -> if (this.isNotNull) "rs.getLong(\"${this.name}\")" else "(Long) Optional.ofNullable(rs.getObject(\"${this.name}\")).orElse(null)"
-        "integer", "int" -> if (this.isNotNull) "rs.getInt(\"${this.name}\")" else "(Integer) Optional.ofNullable(rs.getObject(\"${this.name}\")).orElse(null)"
-        "float" -> if (this.isNotNull) "rs.getFloat(\"${this.name}\")" else "(Float) Optional.ofNullable(rs.getObject(\"${this.name}\")).orElse(null)"
-        "double" -> if (this.isNotNull) "rs.getDouble(\"${this.name}\")" else "(Double) Optional.ofNullable(rs.getObject(\"${this.name}\")).orElse(null)"
-        "bit" -> if (this.isNotNull) "rs.getBoolean(\"${this.name}\")" else "(Boolean) Optional.ofNullable(rs.getObject(\"${this.name}\")).orElse(null)"
-        "varchar", "char", "text" -> "rs.getString(\"${this.name}\")"
-        "timestamp" -> "rs.getTimestamp(\"${this.name}\").toInstant()" //TODO: Support nullable timestamp
-        else -> "rs.getObject(\"${this.name}\")"
-    }
-}
-
-fun DbTable.javaRowMapper(): String {
-    val daoName = this.name.snakeToUpperCamelCase()
-    val sb = StringBuilder("" +
-        "    private $daoName mapRow(ResultSet rs, int rowNum) throws SQLException {\n" +
-        "        return ${daoName}.builder()\n")
-
-    for (column in DasUtil.getColumns(this)) {
-        sb.append("${" ".repeat(16)}.${column.name.snakeToLowerCamelCase()}(${column.javaColumnMapper()})\n")
-    }
-    sb.append("${" ".repeat(16)}.build();\n" +
-        "    }")
-    return sb.toString()
 }
